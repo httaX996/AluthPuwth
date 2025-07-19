2a498cc02c2f89b79918c861ab3b4d6ec7c9c11b
httaX996 <damindakule@gmail.com>
2025-07-19 06:45:39 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,6 +7,6 @@ module.exports = {
 
 SESSION_ID: process.env.SESSION_ID === undefined ? '𝙽𝙴𝙻𝚄𝙼𝙸-𝙼𝙳=3sAETTxR#eR2sLEouOkgzMIvepbBYVAyevcYe-ftT_ugmW0PkMNs' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
-MODE: process.env.MODE === undefined ?"public" : process.env.MODE,
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS
+MODE: process.env.MODE === undefined ?"groups" : process.env.MODE,
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"false" : process.env.AUTO_READ_STATUS
 };
